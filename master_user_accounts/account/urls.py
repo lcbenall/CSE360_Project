72f98798f28b62933d821be21204d0c99747af9f
lcbenall <lcbenall@gmail.com>
2014-11-24 23:42:11 -0700

    Update urls.py

--- master_user_accounts/account/urls.py
+++ master_user_accounts/account/urls.py
@@ -10,13 +10,14 @@ from account.views import SettingsView
 
 urlpatterns = patterns(
     "",
-    url(r"^signup/$", SignupView.as_view(), name="account_signup.urls"),
-    url(r"^login/$", LoginView.as_view(), name="account_login.urls"),
-    url(r"^logout/$", LogoutView.as_view(), name="account_logout.urls"),
-    url(r"^confirm_email/(?P<key>\w+)/$", ConfirmEmailView.as_view(), name="account_confirm_email.urls"),
-    url(r"^password/$", ChangePasswordView.as_view(), name="account_password.urls"),
-    url(r"^password/reset/$", PasswordResetView.as_view(), name="account_password_reset.urls"),
-    url(r"^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$", PasswordResetTokenView.as_view(), name="account_password_reset_token.urls"),
-    url(r"^settings/$", SettingsView.as_view(), name="account_settings.urls"),
-    url(r"^delete/$", DeleteView.as_view(), name="account_delete.urls"),
+    url(r"^signup/$", SignupView.as_view(), name="account_signup"),
+    url(r"^login/$", LoginView.as_view(), name="account_login"),
+    url(r"^logout/$", LogoutView.as_view(), name="account_logout"),
+    url(r"^confirm_email/(?P<key>\w+)/$", ConfirmEmailView.as_view(), name="account_confirm_email"),
+    url(r"^password/$", ChangePasswordView.as_view(), name="account_password"),
+    url(r"^password/reset/$", PasswordResetView.as_view(), name="account_password_reset"),
+    url(r"^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$", PasswordResetTokenView.as_view(), name="account_password_reset_token"),
+    url(r"^settings/$", SettingsView.as_view(), name="account_settings"),
+    url(r"^delete/$", DeleteView.as_view(), name="account_delete"),
+    url(r"^account/", include("account.urls")),
 )
